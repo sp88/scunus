c037656ed663c8972ad50ab113110c2bd9a16905
Sahan Perera <sahan88@gmail.com>
2017-03-30 12:33:24 +0530

    Start and end tone bins

--- app/src/main/java/com/example/sahan/scunus/Constants.java
+++ app/src/main/java/com/example/sahan/scunus/Constants.java
@@ -5,6 +5,8 @@ public class Constants {
     public static final int FFT_LENGTH = 1024;
     public static final int N_FFT_AVERAGE = 1;
     public static final double VALUE_MAX = 32767.0;   // Maximum signal value
+    public static final int START_TONE_BIN = 418; // 418	44100	1024	 18,001.76
+    public static final int END_TONE_BIN = 435; // 435	44100	1024	 18,733.89
     public static final String WINDOW_FUNCTION = "Hanning";
     public static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     public static final String SENDER_ACTIVITY = "com.example.sahan.scunus.SenderActivity";
