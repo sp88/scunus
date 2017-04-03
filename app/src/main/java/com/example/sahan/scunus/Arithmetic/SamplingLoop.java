d4a6d3a9abbe2a76b62e7fa6f75d86a5cb1def9c
Sahan Perera <sahan88@gmail.com>
2017-04-03 14:25:33 +0530

    Clean up

--- app/src/main/java/com/example/sahan/scunus/Arithmetic/SamplingLoop.java
+++ app/src/main/java/com/example/sahan/scunus/Arithmetic/SamplingLoop.java
@@ -23,7 +23,11 @@ import android.util.Log;
 import android.widget.TextView;
 
 import com.example.sahan.scunus.Constants;
+import com.example.sahan.scunus.Demodulator.Demodulator;
 import com.example.sahan.scunus.R;
+
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * Read a snapshot of audio data at a regular interval, and compute the FFT
@@ -41,114 +45,13 @@ public class SamplingLoop extends Thread {
     int audioSourceId = RECORDER_AGC_OFF;
     Activity activity;
     private int scunusCounter = 0;
-//    private final AnalyzerParameters analyzerParam;
-
-//    private SineGenerator sineGen1;
-//    private SineGenerator sineGen2;
-//    private double[] spectrumDBcopy;   // XXX, transfers data from com.example.sahan.scunus.Arithmetic.SamplingLoop to AnalyzerGraphic
-
-//    private final AnalyzerActivity activity;
-
-    volatile double wavSecRemain;
-    volatile double wavSec = 0;
-
     public SamplingLoop(Context context){ //AnalyzerActivity _activity, AnalyzerParameters _analyzerParam) {
         activity = (Activity) context;
-//        activity = _activity;
-//        analyzerParam = _analyzerParam;
-//
-//        isPaused1 = ((SelectorText) activity.findViewById(R.id.run)).getText().toString().equals("stop");
-//        // Signal sources for testing
-//        double fq0 = Double.parseDouble(activity.getString(R.string.test_signal_1_freq1));
-//        double amp0 = Math.pow(10, 1/20.0 * Double.parseDouble(activity.getString(R.string.test_signal_1_db1)));
-//        double fq1 = Double.parseDouble(activity.getString(R.string.test_signal_2_freq1));
-//        double amp1 = Math.pow(10, 1/20.0 * Double.parseDouble(activity.getString(R.string.test_signal_2_db1)));
-//        double fq2 = Double.parseDouble(activity.getString(R.string.test_signal_2_freq2));
-//        double amp2 = Math.pow(10, 1/20.0 * Double.parseDouble(activity.getString(R.string.test_signal_2_db2)));
-//        if (analyzerParam.audioSourceId == 1000) {
-//            sineGen1 = new SineGenerator(fq0, analyzerParam.sampleRate, analyzerParam.SAMPLE_VALUE_MAX * amp0);
-//        } else {
-//            sineGen1 = new SineGenerator(fq1, analyzerParam.sampleRate, analyzerParam.SAMPLE_VALUE_MAX * amp1);
-//        }
-//        sineGen2 = new SineGenerator(fq2, analyzerParam.sampleRate, analyzerParam.SAMPLE_VALUE_MAX * amp2);
     }
-
-    private void SleepWithoutInterrupt(long millis) {
-        try {
-            Thread.sleep(millis);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private double baseTimeMs = SystemClock.uptimeMillis();
-
-    private void LimitFrameRate(double updateMs) {
-        // Limit the frame rate by wait `delay' ms.
-        baseTimeMs += updateMs;
-        long delay = (int) (baseTimeMs - SystemClock.uptimeMillis());
-//      Log.i(TAG, "delay = " + delay);
-        if (delay > 0) {
-            try {
-                Thread.sleep(delay);
-            } catch (InterruptedException e) {
-                Log.i(TAG, "Sleep interrupted");  // seems never reached
-            }
-        } else {
-            baseTimeMs -= delay;  // get current time
-            // Log.i(TAG, "time: cmp t="+Long.toString(SystemClock.uptimeMillis())
-            //            + " v.s. t'=" + Long.toString(baseTimeMs));
-        }
-    }
-
-    private double[] mdata;
-
-    // Generate test data.
-//    private int readTestData(short[] a, int offsetInShorts, int sizeInShorts, int id) {
-//        if (mdata == null || mdata.length != sizeInShorts) {
-//            mdata = new double[sizeInShorts];
-//        }
-//        Arrays.fill(mdata, 0.0);
-//        switch (id - 1000) {
-//            case 1:
-//                sineGen2.getSamples(mdata);
-//                // No break, so values of mdata added.
-//            case 0:
-//                sineGen1.addSamples(mdata);
-//                for (int i = 0; i < sizeInShorts; i++) {
-//                    a[offsetInShorts + i] = (short) Math.round(mdata[i]);
-//                }
-//                break;
-//            case 2:
-//                for (int i = 0; i < sizeInShorts; i++) {
-//                    a[i] = (short) (analyzerParam.SAMPLE_VALUE_MAX * (2.0*Math.random() - 1));
-//                }
-//                break;
-//            default:
-//                Log.w(TAG, "readTestData(): No this source id = " + analyzerParam.audioSourceId);
-//        }
-//        // Block this thread, so that behave as if read from real device.
-//        LimitFrameRate(1000.0*sizeInShorts / analyzerParam.sampleRate);
-//        return sizeInShorts;
-//    }
 
     @Override
     public void run() {
         AudioRecord record;
-//        Thread.currentThread().setPriority(Thread.MAX_PRIORITY);
-
-//        long tStart = SystemClock.uptimeMillis();
-//        try {
-//            activity.graphInit.join();  // TODO: Seems not working as intended....
-//        } catch (InterruptedException e) {
-//            Log.w(TAG, "run(): activity.graphInit.join() failed.");
-//        }
-//        long tEnd = SystemClock.uptimeMillis();
-//        if (tEnd - tStart < 500) {
-//            Log.i(TAG, "wait more.." + (500 - (tEnd - tStart)) + " ms");
-//            // Wait until previous instance of AudioRecord fully released.
-//            SleepWithoutInterrupt(500 - (tEnd - tStart));
-//        }
 
         int minBytes = AudioRecord.getMinBufferSize(Constants.SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
                 AudioFormat.ENCODING_PCM_16BIT);
@@ -173,13 +76,8 @@ public class SamplingLoop extends Thread {
         // The buffer size here seems not relate to the delay.
         // So choose a larger size (~1sec) so that overrun is unlikely.
         try {
-//            if (analyzerParam.audioSourceId < 1000) {
-//                record = new AudioRecord(analyzerParam.audioSourceId, analyzerParam.sampleRate, AudioFormat.CHANNEL_IN_MONO,
-//                        AudioFormat.ENCODING_PCM_16BIT, analyzerParam.BYTE_OF_SAMPLE * bufferSampleSize);
-//            } else {
-                record = new AudioRecord(RECORDER_AGC_OFF, Constants.SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
+            record = new AudioRecord(RECORDER_AGC_OFF, Constants.SAMPLE_RATE, AudioFormat.CHANNEL_IN_MONO,
                         AudioFormat.ENCODING_PCM_16BIT, BYTE_OF_SAMPLE * bufferSampleSize);
-//            }
         } catch (IllegalArgumentException e) {
             Log.e(TAG, "Fail to initialize recorder.");
 //            activity.analyzerViews.notifyToast("Illegal recorder argument. (change source)");
@@ -193,7 +91,6 @@ public class SamplingLoop extends Thread {
                 String.format("  read chunk size : %d samples, %d Bytes\n", readChunkSize, BYTE_OF_SAMPLE*readChunkSize) +
                 String.format("  FFT length      : %d\n", Constants.FFT_LENGTH) +
                 String.format("  nFFTAverage     : %d\n", Constants.N_FFT_AVERAGE));
-//        analyzerParam.sampleRate = record.getSampleRate();
 
         if (record.getState() == AudioRecord.STATE_UNINITIALIZED) {
             Log.e(TAG, "Arithmetic.SamplingLoop::run(): Fail to initialize AudioRecord()");
@@ -207,23 +104,7 @@ public class SamplingLoop extends Thread {
 
         stft = new STFT(Constants.FFT_LENGTH, Constants.SAMPLE_RATE, Constants.WINDOW_FUNCTION);
         stft.setAWeighting(false);
-//        if (spectrumDBcopy == null || spectrumDBcopy.length != analyzerParam.fftLen/2+1) {
-//            spectrumDBcopy = new double[analyzerParam.fftLen/2+1];
-//        }
 
-//        RecorderMonitor recorderMonitor = new RecorderMonitor(analyzerParam.sampleRate, bufferSampleSize, "com.example.sahan.scunus.Arithmetic.SamplingLoop::run()");
-//        recorderMonitor.start();
-
-//      FPSCounter fpsCounter = new FPSCounter("com.example.sahan.scunus.Arithmetic.SamplingLoop::run()");
-
-//        WavWriter wavWriter = new WavWriter(analyzerParam.sampleRate);
-//        boolean bSaveWavLoop = activity.bSaveWav;  // change of bSaveWav during loop will only affect next enter.
-//        if (bSaveWavLoop) {
-//            wavWriter.start();
-//            wavSecRemain = wavWriter.secondsLeft();
-//            wavSec = 0;
-//            Log.i(TAG, "PCM write to file " + wavWriter.getPath());
-//        }
 
         // Start recording
         try {
@@ -235,33 +116,12 @@ public class SamplingLoop extends Thread {
         }
 
         // Main loop
-        // When running in this loop (including when paused), you can not change properties
-        // related to recorder: e.g. audioSourceId, sampleRate, bufferSampleSize
-        // TODO: allow change of FFT length on the fly.
         scunusCounter = 0;
+        List<Integer> signalBins = new LinkedList<>();
         while (isRunning) {
+            long startTime = System.currentTimeMillis();
             // Read data
-//            if (analyzerParam.audioSourceId >= 1000) {
-//                numOfReadShort = readTestData(audioSamples, 0, readChunkSize, analyzerParam.audioSourceId);
-//            } else {
-                numOfReadShort = record.read(audioSamples, 0, readChunkSize);   // pulling
-//            }
-//            if ( recorderMonitor.updateState(numOfReadShort) ) {  // performed a check
-//                if (recorderMonitor.getLastCheckOverrun())
-//                    activity.analyzerViews.notifyOverrun();
-//                if (bSaveWavLoop)
-//                    wavSecRemain = wavWriter.secondsLeft();
-//            }
-//            if (bSaveWavLoop) {
-//                wavWriter.pushAudioShort(audioSamples, numOfReadShort);  // Maybe move this to another thread?
-//                wavSec = wavWriter.secondsWritten();
-//                activity.analyzerViews.updateRec(wavSec);
-//            }
-//            if (isPaused1) {
-//          fpsCounter.inc();
-                // keep reading data, for overrun checker and for write wav data
-//                continue;
-//            }
+            numOfReadShort = record.read(audioSamples, 0, readChunkSize);
 
             stft.feedData(audioSamples, numOfReadShort);
 
@@ -269,20 +129,6 @@ public class SamplingLoop extends Thread {
             if (stft.nElemSpectrumAmp() >= Constants.N_FFT_AVERAGE) {
                 // Update spectrum or spectrogram
                 final double[] spectrumDB = stft.getSpectrumAmpDB();
-//                Log.e("spectrum length", spectrumDB.length+"");
-//                Log.e("spectrum 1st",  spectrumDB[0]+"");
-//                Log.e("spectrum last", spectrumDB[spectrumDB.length-1]+"");
-//                if(spectrumDB[427] > -20.0){
-//                    Log.e("423th bin", String.valueOf( spectrumDB[423] ));
-//                    Log.e("424th bin", String.valueOf( spectrumDB[424] ));
-//                    Log.e("425th bin", String.valueOf( spectrumDB[425] ));
-//                    Log.e("435th bin", String.valueOf( spectrumDB[435] ));
-//                }
-//                Log.e("spectrum", Arrays.toString(spectrumDB));
-//                System.arraycopy(spectrumDB, 0, spectrumDBcopy, 0, spectrumDB.length);
-//                activity.analyzerViews.update(spectrumDBcopy);
-//          fpsCounter.inc();
-
                 stft.calculatePeak();
 //                if(stft.maxAmpFreq > 17990 && stft.maxAmpFreq < 18010) {
 //                    Log.e("max Freq", String.valueOf(stft.maxAmpFreq));
@@ -304,7 +150,7 @@ public class SamplingLoop extends Thread {
                 double maxPower = spectrumDB[Constants.START_TONE_BIN];
 
                 for(int i = Constants.START_TONE_BIN; i <= Constants.END_TONE_BIN; i++){
-                    if( (Math.abs(Math.abs(spectrumDB[i]) - Math.abs(rms)) < 5) &&
+                    if( (Math.abs(Math.abs(spectrumDB[i]) - Math.abs(rms)) < 10) &&
                             (spectrumDB[i] >= maxPower)){
                         maxPowerBin = i;
                         maxPower = spectrumDB[i];
@@ -313,16 +159,15 @@ public class SamplingLoop extends Thread {
 
                 if(maxPowerBin != -1){
                     Log.e("Bin", String.valueOf(maxPowerBin));
+                    signalBins.add(maxPowerBin);
                     scunusCounter++;
                 }
 
-//                activity.maxAmpFreq = stft.maxAmpFreq;
-//                activity.maxAmpDB = stft.maxAmpDB;
-
-                // get RMS
-//                activity.dtRMS = stft.getRMS();
-//                activity.dtRMSFromFT = stft.getRMSFromFT();
             }
+            long endTime = System.currentTimeMillis();
+            Log.i("startTime", String.format("%d", startTime));
+            Log.i("endTime", String.format("%d", endTime));
+            Log.i("loop duration", String.format("%d", endTime - startTime));
         } // END - while(isRunning)
 
 //        Log.i(TAG, "Arithmetic.SamplingLoop::Run(): Actual sample rate: " + recorderMonitor.getSampleRate());
@@ -330,26 +175,11 @@ public class SamplingLoop extends Thread {
         record.stop();
         record.release();
         Log.e("ScunusCount", String.valueOf(scunusCounter));
-//        if (bSaveWavLoop) {
-//            Log.i(TAG, "com.example.sahan.scunus.Arithmetic.SamplingLoop::Run(): Ending saved wav.");
-//            wavWriter.stop();
-//            activity.analyzerViews.notifyWAVSaved(wavWriter.relativeDir);
-//        }
+//        Demodulator demodulator = new Demodulator();
+//        demodulator.demodulate(signalBins);
+
     }
 
-    void setAWeighting(boolean isAWeighting) {
-        if (stft != null) {
-            stft.setAWeighting(isAWeighting);
-        }
-    }
-
-    void setPause(boolean pause) {
-        this.isPaused1 = pause;
-    }
-
-    boolean getPause() {
-        return this.isPaused1;
-    }
 
     public void finish() {
         isRunning = false;
